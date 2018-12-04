fc64d93cb8f5c9268e3ccd7f1e81f7979069ae7a
michinori <andrias1936@basil.ocn.ne.jp>
2018-12-04 18:28:09 +0900

    コードの整理。ファイル名をいちいち入力するのが面倒なので、prevDat.datというファイルに前回入力した日付やイメージソースを記入し、そこから読み取れる設定を追加。

--- analyzer.c
+++ analyzer.c
@@ -3,61 +3,109 @@
 
 #define FLP 40000
 
-int main(int argc, char *argv[])
-{
-  int targetIndex;
-  FILE *data;
-  float posx[FLP];
-  float posy[FLP];
-  float vx[FLP];
-  float vy[FLP];
-  float vabs[FLP];
-  float rho[FLP];
-  float pressure[FLP];
+typedef struct{
+  float px; 
+  float py;
+  float vx;
+  float vy;
+  float vabs;
+  float ax;
+  float ay;
+  float rho; //density
+  float pressure; //pressure
+}pState;
 
-  char imgName[128];
-  char fName[256];
-  char tmpStr[256];
+void writePrevName(char imgName[], int date[]){
+  FILE *fp;
+  char *filename="prevDat.dat";
+  char stringline[256];
+  fp=fopen(filename, "w");
+  sprintf(stringline, "%s %d %d", imgName, date[0], date[1]);
+  fprintf(fp, "%s\n", stringline);
+  fclose(fp);
+}
 
-  int date[2];
-  int i;
-  int spaceCount=0;
-  int indexCount=0;
-  int particleIndex=0;
-  for(i=0; i<2; i++){
-    date[i]=0;
+void getPrevName(char imgName[], int date[]){
+  FILE *fp;
+  char *filename="prevDat.dat";
+  char readline[256];
+  fp=fopen(filename,"r");
+  if(fp==NULL){
+    fprintf(stderr, "prevDat.dat does not exist!\n");
+    exit(0);
   }
 
-  
+  fgets(readline, 256, fp);
+  sscanf(readline, "%s %d %d", imgName, &date[0], &date[1]);
+  fprintf(stderr, "%s %d %d\n", imgName, date[0], date[1]);
+  fclose(fp);
+}
+
+void inputFileNames(char imgName[], int date[]){
   fprintf(stderr, "Enter month-date:");
   scanf ("%d", &date[0]);
   fprintf(stderr, "Enter hour-minute:");
   scanf ("%d", &date[1]);
   fprintf(stderr, "Enter source image name:");
   scanf ("%s", imgName);
+}
+
+int main(void)
+{
+  int targetIndex;
+  FILE *data;
+
+  char imgName[128];
+  char fName[256];
+  char tmpStr[256];
+
+  pState p[FLP];
+  int date[2];
+  int i;
+  int spaceCount=0;
+  int indexCount=0;
+  int particleIndex=0;
+  
+  for(i=0; i<2; i++){
+    date[i]=0;
+  }
+  for(i=0; i<FLP; i++){
+    p[i].px=0;
+    p[i].py=0;
+    p[i].vx=0;
+    p[i].vy=0;
+    p[i].vabs=0;
+    p[i].rho=0; 
+    p[i].pressure=0;
+  }
+
+  fprintf(stderr, "1:enter file name\n");
+  fprintf(stderr, "2:use previous settings\n");
+  scanf("%d", &i);
+
+  if(i==1){
+    inputFileNames(imgName, date);
+  }else if(i==2){
+    getPrevName(imgName, date);
+  }else{
+    return 0;
+  }
 
   fprintf(stderr, "Enter target index:\n");
   scanf("%d", &targetIndex);
-
-  for(i=0; i<2; i++){
+ 
+ for(i=0; i<2; i++){
     fprintf(stderr, "%d\n", date[i]);
   }
   fprintf(stderr, "%s\n", imgName);
 
+  //writing dat file name
   sprintf(fName, "./Source_%s/%s_0.00_%d_%d_data.dat", imgName, imgName, date[0], date[1]);
   fprintf(stderr, "%s\n", fName);
 
-  for(i=0; i<FLP; i++){
-    posx[i]=0;
-    posy[i]=0;
-    vx[i]=0;
-    vy[i]=0;
-    vabs[i]=0;
-    rho[i]=0; 
-    pressure[i]=0;
-  }
-  fprintf(stderr, "data array initialized\n");
+  writePrevName(imgName, date);
 
+  //file opening
   data=fopen(fName, "r");
   if(data==NULL){
     fprintf(stderr, "File cannnot be opened!\n");
@@ -73,35 +121,31 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
+  //getting data at target index
   while(fgets(tmpStr, 256, data)!=NULL){
     if(*tmpStr=='\n'){
       spaceCount++;
-      }
+    }
 
+    //reach target index
     if(indexCount==targetIndex && spaceCount==0){
       fprintf(stderr, "%s", tmpStr);
-      sscanf(tmpStr, "%*d %f %f %f %f %f %f %f %*f %*f", &posx[particleIndex], &posy[particleIndex],
-             &vx[particleIndex], &vy[particleIndex], &vabs[particleIndex],
-             &rho[particleIndex], &pressure[particleIndex]);
-    
+      sscanf(tmpStr, "%*d %f %f %f %f %f %f %f %*f %*f", &p[particleIndex].px, &p[particleIndex].py,
+             &p[particleIndex].vx, &p[particleIndex].vy, &p[particleIndex].vabs,
+             &p[particleIndex].rho, &p[particleIndex].pressure);
       particleIndex++;
     }
- 
-     if(spaceCount==2){
+
+    if(spaceCount==2){
       indexCount++;
       spaceCount=0;
-     }
-     
+    }
     if(indexCount>targetIndex){break;}
-
-    //fprintf(stderr, "%s", tmpStr);
-
   }
   fprintf(stderr, "\nparticleIndex:%d index:%d\n", particleIndex,indexCount);
   for(i=0; i<3; i++){
-    fprintf(stderr, "%f %f\n", posx[i], posy[i]);
-}
-
+    fprintf(stderr, "%f %f\n", p[i].px, p[i].py);
+  }
 
   fclose(data);
   return 0;
